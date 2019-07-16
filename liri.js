9c3d3a2bd7355a4730333aaca4b612bdd24c162f
Amy Wolf <awolgemuth@trinisys.loc>
2019-07-16 16:53:12 -0500

    added jason pkg, global variables, switch function

--- liri.js
+++ liri.js
@@ -1,21 +1,32 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 
-//You should then be able to access your keys information like so
+//Spotify API Set-up
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-// Make it so liri.js can take in one of the following commands:
-//    concert-this
-//    spotify-this-song
-//    movie-this
-//    do-what-it-says
+var axios = require("axios");
+var fs = require("fs");
 
-// 2. node liri.js spotify-this-song '<song name here>'
-//    * This will show the following information about the song in your terminal/bash window
-//      * Artist(s)
-//      * The song's name
-//      * A preview link of the song from Spotify
-//      * The album that the song is from
-//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
-//    * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
+//Globally declared variables
+var whatToDo = process.argv[2];
+var searchThis = process.argv.slice(3).join(" ");
+var divider = "\n------------------------------------------------------";
 
+function liri() {
+    switch (whatToDo) {
+        case "concert-this":
+            break;
+
+        case "spotify-this-song":
+            break;
+
+        case "movie-this":
+            break;
+
+        case "do-what-it-says":
+            break;
+
+    }
+}
+liri();
