ccf31c1c58ba3298d6bc7005a7dbfcf529d848bc
Amy Wolf <awolgemuth@trinisys.loc>
2019-07-17 13:08:06 -0500

    Added the 3 other functions spotify-this-song, movie-this, do-what-it-says. Fixed empty search

--- liri.js
+++ liri.js
@@ -11,25 +11,82 @@ var fs = require("fs");
 //Globally declared variables
 var command = process.argv[2];
 var input = process.argv.slice(3).join(" ");
-var divider = "\n------------------------------------------------------";
+var divider = "\n=============================";
 
-function concertIt(bandQuery){
-    var URL = "https://rest.bandsintown.com/artists/" + bandQuery + "/events?app_id=codingbootcamp"
-            axios.get(URL).then(function (response) {
-                var result = response.data[0];
-
-                var bandData = [
-                    "Name: " + result.venue.name,
-                    "Location: " + result.venue.city + ", " + result.venue.region,
-                    "Date: " + result.datetime
-                ].join("\n\n");
-
-                fs.appendFile("log.txt", bandData + divider, function (err) {
-                    if (err) throw err;
-                    console.log(bandData);
-                });
-            })
+function concertIt(bandSearch) {
+    if (bandSearch == "") {
+        bandSearch = "Jennifer Lopez";
+    }
+    var URL = "https://rest.bandsintown.com/artists/" + bandSearch + "/events?app_id=codingbootcamp"
+    axios.get(URL).then(function (response) {
+        var result = response.data[0];
+        var bandData = [
+            "\nVenue Name: " + result.venue.name,
+            "\Venue Location: " + result.venue.city + ", " + result.venue.region,
+            "\Concert Date: " + result.datetime
+        ].join("\n\n");
+        fs.appendFile("log.txt", bandData + divider, function (err) {
+            if (err) throw err;
+            console.log(bandData);
+        });
+    })
 }
+
+function spotifyIt(musicSearch) {
+    //  * If no song is provided then your program will default to "The Sign" by Ace of Base.
+    if (musicSearch == "") {
+        musicSearch = "The Sign Ace of Base";
+    }
+    spotify.search({ type: 'track', query: musicSearch }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        else {
+            for (i = 0; i < data.tracks.items.length && i < 5; i++) {
+                var musicQuery = data.tracks.items[i];
+                // console.log("===============================");
+                // * Artist(s)
+                console.log("Artist: " + musicQuery.artists[0].name +
+                    // * The song's name
+                    "\nSong Name: " + musicQuery.name +
+                    //* A preview link of the song from Spotify
+                    "\nLink to Song: " + musicQuery.preview_url +
+                    //* The album that the song is from
+                    "\nAlbum Name: " + musicQuery.album.name +
+                    "\n===============================");
+            }
+        };
+    });
+}
+
+function movieIt(movieSearch) {
+    var movieChoice;
+    if (movieSearch == "") {
+        console.log(movieSearch)
+        movieChoice = "Jumanji";
+    } else {
+        movieChoice = movieSearch
+    }
+    var URL = "http://www.omdbapi.com/?t=" + movieChoice + "&y=&plot=short&apikey=trilogy";
+    axios.get(URL).then(function (response) {
+        var result = response.data;
+        var movieData = [
+            "Movie: " + result.Title,
+            "Year: " + result.Year,
+            "IMDB Rating: " + result.Ratings[0].Value,
+            "Rotten Tomatoes Rating: " + result.Ratings[0].Value,
+            "Country: " + result.Country,
+            "Language: " + result.Language,
+            "Plot: " + result.Plot,
+            "Actors: " + result.Actors
+        ].join("\n");
+        fs.appendFile("log.txt", movieData + divider, function (err) {
+            if (err) throw err;
+            console.log(movieData);
+        });
+    })
+};
+
 
 var liri = function (whatToDo, searchThis) {
     switch (whatToDo) {
@@ -38,14 +95,31 @@ var liri = function (whatToDo, searchThis) {
             break;
 
         case "spotify-this-song":
+            spotifyIt(searchThis);
             break;
 
         case "movie-this":
+            movieIt(searchThis);
             break;
 
         case "do-what-it-says":
+            doWhatItSays();
             break;
-
     }
 }
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.split(",");
+        dataArr[1] = dataArr[1].replace(/["]+/g, '');
+        whatToDo = dataArr[0];
+        searchThis = dataArr[1];
+        liri (whatToDo, searchThis)
+    });
+
+}
+
 liri (command, input);
